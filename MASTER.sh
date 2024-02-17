07228d943acf4adffe9526be70d329ff13a14377
Thomas Cornish <tomcornish1996@gmail.com>
2024-02-17 18:51:40 +0000

    Now have separate functions for submitting Python and bash scripts

--- MASTER.sh
+++ MASTER.sh
@@ -19,12 +19,29 @@ function getID () {
     tail -3 $jobfile | head -1 | awk -F'python-' '{ print $NF }' | awk -F'.out' '{ print $1 }'
 }
 
-# Function to to submit a job to the queue.
+
+# Function to to submit a bash-scripted job to the queue.
+# Expects two arguments plus an optional third:
+#   $1: Arguments and their values passed to addqueue.
+#   $2: Name of the script being run.
+#   $3: (Optional) Arguments to pass to the bash script itself.
+function submit_job () {
+    if [ -f $jobfile ]
+    then
+        jobID=$(getID)
+        addqueue $1 --runafter $jobID $2 $3 > $jobfile
+    else
+        addqueue $1 $2 $3 > $jobfile
+    fi    
+}
+
+
+# Function to to submit a Python job to the queue.
 # Expects two arguments plus an optional third:
 #   $1: Arguments and their values passed to addqueue.
 #   $2: Name of the script being run.
 #   $3: (Optional) Arguments to pass to the python script itself.
-function submit_job () {
+function submit_pyjob () {
     if [ -f $jobfile ]
     then
         jobID=$(getID)
@@ -71,22 +88,22 @@ function power_spectra_job () {
 
 
 ### downloading data
-#cd data_query/ && submit_job "-q cmb -m 10" get_data.py; cd ..
+#cd data_query/ && submit_pyjob "-q cmb -m 10" get_data.py; cd ..
 
 ### splitting the metadata according to field (and possibly filter)
-#submit_job "-q cmb -m 20" split_metadata.py
+#submit_pyjob "-q cmb -m 20" split_metadata.py
 
 ### applying various cuts to clean the catalogues
-#submit_job "-q cmb -m 40" clean_catalogues.py
+#submit_pyjob "-q cmb -m 40" clean_catalogues.py
 
 ### making maps from the catalogue data
-#submit_job "-q cmb -m 40" make_maps_from_catalogue.py
+#submit_pyjob "-q cmb -m 40" make_maps_from_catalogue.py
 
 ### making maps from the frame metadata
 # metamaps_job "-q cmb -n 1x20 -m 5 -s" make_maps_from_metadata.py
 
 ### making galaxy count and overdensity maps in tomographic bins
-#submit_job "-q cmb -m 40" make_galaxy_maps.py
+#submit_pyjob "-q cmb -m 40" make_galaxy_maps.py
 
 ### computing power spectra; function takes as arguments...
 ###     1: number of cores to use
