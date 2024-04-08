Fixed calculation of menasq in MaskData - was previously calculating the mean and then squaring, but should have been calculating the mean of the squared mask

map_utils.py
@@ -395,5 +395,5 @@ class MaskData:
 		#compute the sum, mean, and mean squared of the mask
 		self.sum = np.sum(self.mask)
 		self.mean = np.mean(self.mask)
-		self.meansq = self.mean ** 2.
+		self.meansq = np.mean(self.mask ** 2.)
 