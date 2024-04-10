242251dd80ccd54302f2268a562fceee2afdee98
Thomas Cornish <tomcornish1996@gmail.com>
2024-04-10 16:50:24 +0100

    Added MASTER shell script for running the pipeline on NERSC

---
+++ MASTER_NERSC.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+###################################################################
+# Configure and run this file when running the pipeline on NERSC. #
+###################################################################
+
+module load conda
+conda activate phsc3
+
+cd slurm_scripts
+
+#uncomment any stages you want to run
+jobs=(
+    #'get_data'
+    'split_metadata'
+    #'clean_catalogues'
+    #'make_maps_from_catalogue'
+    #'make_maps_from_metadata'
+    #'make_galaxy_maps'
+    #'compute_power_spectra'
+    #'make_txpipe_inputs'
+)
+
+#run first job and get job ID
+jid=$(sbatch ${jobs[0]}.sh | cut -d ' ' -f4)
+
+for j in ${jobs[@]:1}
+    do
+        jid=$(sbatch --dependency=afterok:${jid} ${j}.sh | cut -d ' ' -f4)
+    done
+
+cd ..
