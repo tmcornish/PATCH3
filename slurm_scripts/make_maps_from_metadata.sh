1e72564d6d8250950dd72fb25e3fc43045e448a2
Thomas Cornish <tomcornish1996@gmail.com>
2024-04-10 16:12:15 +0100

    Added sbatch script for running make_maps_from_metadata.py (untested)

---
+++ slurm_scripts/make_maps_from_metadata.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=256
+#SBATCH --time=48:00:00
+#SBATCH --constraint=cpu
+#SBATCH --qos=normal
+#SBATCH --account=m1727
+
+cd ..
+
+#see if pipeline configured to split metadata
+if $(python -c "import config as cf; print(cf.makeMapsFromMetadata.split_by_band)")="True"
+then
+    #get the band and run the script for each one
+    for b in $(python -c "import config as cf; print(' '.join(cf.cf_global.bands))")
+    do
+        srun python -u make_maps_from_metadata.py $b
+    done
+else
+    #get the list of all bands and run them simultaneously
+    b=$(python -c "import config as cf; print(','.join(cf.cf_global.bands))")
+    srun python -u make_maps_from_metadata.py $b
+fi
+
+cd slurm_scripts
