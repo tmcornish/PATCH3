3962a26f6b6cd0c9735102c7ea182c511e14df68
Thomas Cornish <tomcornish1996@gmail.com>
2024-04-15 17:28:20 +0100

    Initialised script for PCA

---
+++ pca_systematics.py
@@ -0,0 +1,25 @@
+#####################################################################################################
+# - 
+#####################################################################################################
+
+import config
+import healpy as hp
+import healsparse as hsp
+import numpy as np
+from map_utils import *
+import plot_utils as pu
+from matplotlib import pyplot as plt
+
+### SETTINGS ###
+cf = config.pcaSystematics
+plt.style.use(pu.styledict)
+
+
+###################
+#### FUNCTIONS ####
+###################
+
+#######################################################
+###############    START OF SCRIPT    #################
+#######################################################
+
