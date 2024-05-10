355a995367266a4b4844e6774bc2158399cd1bee
Thomas Cornish <tomcornish1996@gmail.com>
2024-05-10 15:48:17 +0100

    Removed error_message from output_utils, since now in gen_utils

--- output_utils.py
+++ output_utils.py
@@ -106,26 +106,6 @@ def array_to_fits(data, filename, CTYPE1='RA', CTYPE2='DEC', CRPIX=[1,1], CRVAL=
 	hdul.writeto(filename, overwrite=True)
 
 
-def error_message(module, message):
-	'''
-	Prints a nicely formatted error message. For use within other modules as a means of identifying
-	issues. 
-
-	Parameters
-	----------
-	module: str
-		The name of the module being debugged.
-
-	message: str
-		The body of the error message to print.
-	'''
-	err_str = [
-		colour_string(f'{module}\n', 'cyan'),
-		colour_string('Error: '),
-		colour_string(message, 'white')]
-	print(''.join(err_str))
-
-
 
 def write_output(t, fname, **kwargs):
 	'''
