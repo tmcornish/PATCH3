a389584817af69179d33dbf5bc48c67fa111b16d
Thomas Cornish <tomcornish1996@gmail.com>
2024-05-21 14:04:05 +0100

    cl_guess and covariances computed with and without deprojection

--- amend_power_spectra.py
+++ amend_power_spectra.py
@@ -58,24 +58,50 @@ for fd in cf.get_global_fields():
 	outfile_main = f'{cf.PATH_OUT}{fd}/{cf.outfile}'
 	for p in pairings:
 		i,j = [int(x) for x in p.strip('()').split(',')]
-		outfile_now = f'{outfile_main[:-5]}_{i}_{j}.hdf5'
+		outfile_ii = f'{outfile_main[:-5]}_{i}_{i}.hdf5'
+		outfile_ij = f'{outfile_main[:-5]}_{i}_{j}.hdf5'
+		outfile_jj = f'{outfile_main[:-5]}_{j}_{j}.hdf5'
 
 		f_i = density_fields[i]
 		f_j = density_fields[j]
 		#compute c_ells without deprojection
 		cl_coupled_ii = nmt.compute_coupled_cell(f_i, f_i)
-		cl_coupled_ij = nmt.compute_coupled_cell(f_i, f_j)
+		cl_coupled_nd = nmt.compute_coupled_cell(f_i, f_j)
 		cl_coupled_jj = nmt.compute_coupled_cell(f_j, f_j)
 		#estimate true power spectra
 		cl_guess_ii = cl_coupled_ii / mu_w2
-		cl_guess_ij = cl_coupled_ij / mu_w2
+		cl_guess_ij = cl_coupled_nd / mu_w2
 		cl_guess_jj = cl_coupled_jj / mu_w2
 
-		#decouple the c_ells (note this is without deprojection)
-		cl_decoupled = w.decouple_cell(cl_coupled_ij)
+		#decouple the c_ells (no deprojection)
+		cl_decoupled_nd = w.decouple_cell(cl_coupled_nd)
 
 		#recompute caussian covariance matrix
-		print('Calculating covariance matrix...')
+		print('Calculating covariance matrix (no deprojection)...')
+		covar_nd = nmt.gaussian_covariance(cw,
+								  		0, 0, 0, 0,
+										[cl_guess_ii[0]],
+										[cl_guess_ij[0]],
+										[cl_guess_ij[0]],
+										[cl_guess_jj[0]],
+										w)
+		err_cell_nd = np.diag(covar_nd) ** 0.5
+		print('Done!')
+
+
+		#load the coupled c_ells with deprojection
+		with h5py.File(outfile_ii, 'r') as psfile:
+			cl_coupled_ii = psfile[f'{i},{i}/cl_coupled'][...]
+		with h5py.File(outfile_ij, 'r') as psfile:
+			cl_coupled = psfile[f'{i},{j}/cl_coupled'][...]
+		with h5py.File(outfile_jj, 'r') as psfile:
+			cl_coupled_jj = psfile[f'{j},{j}/cl_coupled'][...]
+		#use these to estimate true c_ells with deprojection
+		cl_guess_ii = cl_coupled_ii / mu_w2
+		cl_guess_ij = cl_coupled / mu_w2
+		cl_guess_jj = cl_coupled_jj / mu_w2
+		#recompute caussian covariance matrix
+		print('Calculating covariance matrix (with deprojection)...')
 		covar = nmt.gaussian_covariance(cw,
 								  		0, 0, 0, 0,
 										[cl_guess_ii[0]],
@@ -87,11 +113,12 @@ for fd in cf.get_global_fields():
 		print('Done!')
 
 
-		with h5py.File(outfile_now, 'a') as psfile:
+		with h5py.File(outfile_ij, 'a') as psfile:
 			#populate the output file with the new results
 			gp = psfile[p]
-			_ = gp.create_dataset('cl_coupled_no_deproj', data=cl_coupled_ij)
-			_ = gp.create_dataset('cl_decoupled_no_deproj', data=cl_decoupled)
+			_ = gp.create_dataset('cl_coupled_no_deproj', data=cl_coupled_nd)
+			_ = gp.create_dataset('cl_decoupled_no_deproj', data=cl_decoupled_nd)
+			_ = gp.create_dataset('covar_no_deproj', data=covar_nd)
+			_ = gp.create_dataset('err_cell_no_deproj', data=err_cell_nd)
 			gp['covar'][...] = covar
 			gp['err_cell'][...] = err_cell
-	
