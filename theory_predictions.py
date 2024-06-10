d4debb799650288e798b159b9ce2b8eb99d5a7e5
Thomas Cornish <tomcornish1996@gmail.com>
2024-06-10 15:57:04 +0100

    Script now computes the theory c_ells and saves to a file

--- theory_predictions.py
+++ theory_predictions.py
@@ -1,6 +1,6 @@
 #####################################################################################################
 # - Estimates the n(z) distributions and then computes theoretical predictions 
-#   for the power spectra. 
+#   for the power spectra.
 #####################################################################################################
 
 import pyccl as ccl
@@ -16,7 +16,25 @@ cf = config.theoryPredictions
 #### FUNCTIONS ####
 ###################
 
-def get_nofz(fd, nbins=50, group='', zlims=None):
+def get_nofz(fd, group='', zlims=None):
+	'''
+	Computes estimates of the n(z) distributions in each redshift bin for a given
+	field and returns them in a dictionary.
+
+	Parameters
+	----------
+	fd: str
+		Name of the field being analysed.
+	
+	group: str
+		Group within which the relevant data are expected to reside.
+	
+	Returns
+	-------
+	nofz: dict
+		Dictionary containing the redshift bin centres and the n(z) values in each
+		of those bins for each tomographic bin.
+	'''
 	#define lists to contain the best z estimates and the random MC draws
 	z_best, z_mc = [], []
 
@@ -37,9 +55,9 @@ def get_nofz(fd, nbins=50, group='', zlims=None):
 
 	#determine the bin edges and centres to use for the n(z) histograms
 	if zlims is None:
-		bins = np.linspace(z_best.min(), z_best.max(), (nbins + 1))
+		bins = np.linspace(z_best.min(), z_best.max(), (cf.nbins_nofz + 1))
 	else:
-		bins = np.linspace(*zlims, (nbins + 1))
+		bins = np.linspace(*zlims, (cf.nbins_nofz + 1))
 	bin_centres = (bins[1:] + bins[:-1]) / 2
 	
 	#set up a dictionary for containing the n(z)s for the different bins
@@ -58,3 +76,73 @@ def get_nofz(fd, nbins=50, group='', zlims=None):
 #######################################################
 
 #retrieve the bin pairings
+pairings, pairings_s = cf.get_bin_pairings()
+
+#define the fiducial cosmology
+cosmo = ccl.Cosmology(**cf.cosmo_fiducial)
+
+#range of wavenumbers and scale factors over which theory power spectra will be computed
+lk_arr = np.log(np.geomspace(1E-4, 100, 256))
+a_arr = 1. / (1. + np.linspace(0, 6, 100)[::-1])
+
+#cycle through the specified fields
+for fd in cf.get_global_fields():
+	#retrieve estimates of the n(z) distributions in each bin
+	nofz = get_nofz(fd, group='photometry')
+	
+	#create a dictionary containing the tracers in each redshift bin
+	tracers = {
+		f'bin{i}' : ccl.NumberCountsTracer(
+											cosmo, 
+											has_rsd=False, 
+											dndz=(nofz['z'], nofz[f'bin{i}']), 
+											bias=(nofz['z'], np.ones_like(nofz['z']))
+											)
+		for i in range(len(cf.zbins) - 1)
+	}
+
+	#create a halo model
+	m200def = ccl.halos.MassDef200m							#halo mass definition
+	hmf = ccl.halos.MassFuncTinker08(mass_def=m200def)		#halo mass function
+	hbf = ccl.halos.HaloBiasTinker10(mass_def=m200def)		#halo bias function
+	conc = ccl.halos.ConcentrationDuffy08(mass_def=m200def)	#halo concentration function
+	#feed this information to a HaloMassCalculator
+	hmc = ccl.halos.HMCalculator(
+		mass_function=hmf,
+		halo_bias=hbf,
+		mass_def=m200def
+		)
+	#create halo profile
+	prof = ccl.halos.HaloProfileHOD(mass_def=m200def, concentration=conc)
+	#2pt correlator
+	prof2pt = ccl.halos.Profile2ptHOD()
+
+	#halo-model power spectrum for galaxies
+	pk = ccl.halos.halomod_Pk2D(
+		cosmo,
+		hmc,
+		prof,
+		prof_2pt=prof2pt,
+		prof2=prof,
+		a_arr=a_arr,
+		lk_arr=lk_arr
+	)
+
+	#compute the theory c_ells
+	lmax = 3. * cf.nside_hi - 1
+	ells = np.unique(np.geomspace(1, lmax, 128).astype(int)).astype(float)
+	#open the output file, compute and save the theory cells
+	outfile = f'{cf.PATH_OUT}{fd}/{cf.theory_out}' 
+	with h5py.File(outfile, 'w') as psfile:
+		_ = psfile.create_dataset('ells', data=ells)
+		for i, j in pairings:
+			c_ells = ccl.angular_cl(
+				cosmo,
+				tracers[f'bin{i}'],
+				tracers[f'bin{j}'],
+				ells,
+				p_of_k_a=pk
+			)
+			_ = psfile.create_dataset(f'bin{i}-bin{j}', data=c_ells)
+	
+
