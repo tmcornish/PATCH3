e6abc9544640a53b80f629edf9df88ebca99ee59
Thomas Cornish <tomcornish1996@gmail.com>
2024-09-04 16:13:19 +0100

    Updated the Slurm script for compute_power_spectra - should now use all available CPUs on NERSC nodes

--- slurm_scripts/compute_power_spectra.sh
+++ slurm_scripts/compute_power_spectra.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --cpus-per-task=256
+#SBATCH -N 1
+#SBATCH -C cpu
+#SBATCH -q regular
 #SBATCH --time=24:00:00
-#SBATCH --constraint=cpu
-#SBATCH --qos=regular
-#SBATCH --account=m1727
+#SBATCH -J computePowerSpectra
+#SBATCH --mail-user=thomas.cornish@physics.ox.ac.uk
+#SBATCH --mail-type=ALL
+#SBATCH -A m1727
 
 cd ..
 
-export OMP_PROC_BIND=spread
-export OMP_PLACES=threads
 export OMP_NUM_THREADS=256
+export OMP_PLACES=threads
+export OMP_PROC_BIND=spread
 
-srun python -u compute_power_spectra.py
+srun -n 1 -c 256 --cpu_bind=cores python -u compute_power_spectra.py
 
 cd slurm_scripts
