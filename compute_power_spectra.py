df0480a0224b826aa469b2ef126c3b0c68929879
Thomas Cornish <tomcornish1996@gmail.com>
2024-09-12 16:36:29 +0100

    compute_power_spectra now saves the best-fit coefficients from deprojection to a file in the cache for each z bin

--- compute_power_spectra.py
+++ compute_power_spectra.py
@@ -365,6 +365,14 @@ for fd in cf.get_global_fields():
 				gp['ells_theory'] = h5py.ExternalLink(theory_file, 'ells')
 				gp['cl_theory'] = h5py.ExternalLink(theory_file, f'bin{i}-bin{j}')
 
+	#save the best-fit coefficients from linear deprojection for each field
+	for i,f_i in enumerate(density_fields):
+		alpha_file = PATH_CACHE + f'deproj_alphas_bin{i}.txt'
+		alphas = f_i.alphas
+		alphas_str = '\n'.join(str(alpha) for alpha in alphas)
+		with open(alpha_file, 'w') as af:
+			af.write(alphas_str)
+
 	#compute all covariances if told to AND if density fields have been made 
 	if not cf.var_only and density_fields:
 		print('Computing all covariances...')
