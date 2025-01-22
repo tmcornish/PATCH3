d5407770bdfac38ef61d43cc90631d6b23cd87f6
Thomas Cornish <tomcornish1996@gmail.com>
2025-01-22 18:23:32 +0000

    Updated to correctly incorporate PipelineConfig

--- data_query/get_data.py
+++ data_query/get_data.py
@@ -6,11 +6,12 @@ from astropy.table import Table
 import numpy as np
 #add parent directory to path and import config parameters
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-import config
+from configuration import PipelineConfig as PC
 from output_utils import colour_string
 
 ### SETTINGS ###
-cf = config.getData
+config_file = sys.argv[1]
+cf = PC(config_file, stage='getData')
 
 ################################
 #                              #
@@ -24,7 +25,7 @@ if not os.path.exists(PATH_SQL):
 	os.system(f'mkdir -p {PATH_SQL}')
 
 #Per-frame metadata
-write_frames(cf.dr, f'{PATH_SQL}frames_wide.sql', submit=cf.submit, dir_out=cf.PATH_DATA, do_download=cf.download)
+write_frames(cf.dr, f'{PATH_SQL}frames_wide.sql', submit=cf.submit, dir_out=cf.paths.data, do_download=cf.download)
 
 
 #retrieve the field names and boundary coordinates
@@ -32,7 +33,7 @@ bounds_file = './field_counts_and_boundaries.fits'
 t_bounds = Table.read(bounds_file).to_pandas().set_index('field')
 
 #WIDE fields
-for fd in cf.fields:
+for fd in cf.get_subfields():
 	print(colour_string(fd, 'purple'))
 	fld = fd.encode('utf-8')
 	#get the number of objects
@@ -54,9 +55,9 @@ for fd in cf.fields:
 			decmax_now = decmin_now + ddec
 			
 			#write and submit a query for the data in this sub-field
-			write_fieldsearch(cf.dr, fd, f'{PATH_SQL}run_{fd.upper()}_part{i+1:02d}.sql', dir_out=cf.PATH_DATA, do_photoz=cf.photoz, mizuki_mstar_sfrs=cf.mizuki_mstar_sfrs, submit=cf.submit, apply_cuts=cf.apply_cuts, strict_cuts=cf.strict_cuts, dec_range=[decmin_now,decmax_now], do_download=cf.download, part=i+1)
+			write_fieldsearch(cf.dr, fd, f'{PATH_SQL}run_{fd.upper()}_part{i+1:02d}.sql', dir_out=cf.paths.data, do_photoz=cf.photoz, mizuki_mstar_sfrs=cf.mizuki_mstar_sfrs, submit=cf.submit, apply_cuts=cf.apply_cuts, strict_cuts=cf.strict_cuts, dec_range=[decmin_now,decmax_now], do_download=cf.download, part=i+1)
 	else:
-		write_fieldsearch(cf.dr, fd, f'{PATH_SQL}run_{fd.upper()}.sql', dir_out=cf.PATH_DATA, do_photoz=cf.photoz, mizuki_mstar_sfrs=cf.mizuki_mstar_sfrs, submit=cf.submit, apply_cuts=cf.apply_cuts, strict_cuts=cf.strict_cuts, do_download=cf.download)
+		write_fieldsearch(cf.dr, fd, f'{PATH_SQL}run_{fd.upper()}.sql', dir_out=cf.paths.data, do_photoz=cf.photoz, mizuki_mstar_sfrs=cf.mizuki_mstar_sfrs, submit=cf.submit, apply_cuts=cf.apply_cuts, strict_cuts=cf.strict_cuts, do_download=cf.download)
 
 
 
