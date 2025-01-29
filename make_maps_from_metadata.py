6797dfaaad50584f73489ffc31c77838405113e5
Thomas Cornish <tomcornish1996@gmail.com>
2025-01-29 15:46:10 +0000

    Adapted to yaml-based config

--- make_maps_from_metadata.py
+++ make_maps_from_metadata.py
@@ -2,19 +2,18 @@
 # - Creates maps of various systematics using frame metadata.
 #####################################################################################################
 
-#TODO: write function to create decasu config file from pipeline config info
-#TODO: Integrate properly into pipeline (currently written to be run via SLURM bash script for multiprocessing)
+#TODO: Try to determine a better way of configuring for different machines.
 
-import os, sys
-import config
+import os
+import sys
+from configuration import PipelineConfig as PC
 from decasu.multi_healpix_mapper import MultiHealpixMapper
 from decasu.configuration import Configuration
-from astropy.table import Table
 from output_utils import colour_string
-import glob
 
 ### SETTINGS ###
-cf = config.makeMapsFromMetadata
+config_file = sys.argv[1]
+cf = PC(config_file, stage='makeMapsFromMetadata')
 
 
 
@@ -23,24 +22,21 @@ cf = config.makeMapsFromMetadata
 #######################################################
 
 #retrieve the path of the Decasu config file
-configfile = cf.configfile
+configfile = cf.auxfiles.decasu_config
 #create Configuration object for Decasu
 CONF = Configuration.load_yaml(configfile)
 
-#get the extension of the metadata file and its character length
-ext = cf.metafile.split('.')[-1]
-lenext = len(ext)
 
 #the rest of the script now depends on whether it is being run locally or on glamdring
-if cf.LOCAL:
+if cf.platform == 'local':
 	#number of cores to use
 	import multiprocessing as mp
 	ncores = min(mp.cpu_count()-1, cf.ncores)
 	#cycle through the fields being analysed
-	for fd in cf.get_global_fields():
+	for fd in cf.fields:
 		print(colour_string(fd.upper(), 'orange'))
 		#directory containing outputs for this field
-		OUT = cf.PATH_OUT + fd + '/'
+		OUT = cf.paths.out + fd + '/'
 		PATH_SYST = OUT + 'systmaps/'
 
 		#set up Decasu mapper
@@ -49,35 +45,35 @@ if cf.LOCAL:
 		#check if metadata was split by filter
 		if cf.split_by_band:
 			for b in cf.bands:
-				infile = f'{OUT}{cf.metasplit[:-(lenext+1)]}_{b}.fits'
+				infile = f'{cf.path.data}{cf.data_files.metadata[:-5]}_{fd}_{b}.fits'
 				#run the Decasu mapper for each band individually
 				mapper(infile, bands=b, clear_intermediate_files=True)
 		else:
-			infile = f'{OUT}{cf.metasplit}'
+			infile = f'{cf.path.data}{cf.data_files.metadata[:-5]}_{fd}.fits'
 			bands = ','.join(cf.bands)
 			#run the Decasu mapper for all bands
 			mapper(infile, bands=b, clear_intermediate_files=True)
 
 else:
 	#retrieve the band for which the script is being run from the system arguments
-	b = sys.argv[1]
+	b = sys.argv[2]
 	#environment variable specifying number of CPUs will differ on NERSC and glamdring
-	if cf.NERSC:
+	if cf.platform == 'nersc':
 		import multiprocessing as mp
 		ncores = mp.cpu_count() - 1
 	else:
 		ncores = int(os.getenv('SLURM_NTASKS_PER_NODE'))
 	#cycle through the fields being analysed
-	for fd in cf.get_global_fields():
+	for fd in cf.fields:
 		print(colour_string(fd.upper(), 'orange'))
 		#directory containing outputs for this field
-		OUT = cf.PATH_OUT + fd + '/'
+		OUT = cf.paths.out + fd + '/'
 		PATH_SYST = OUT + 'systmaps/'
 
 		#set up Decasu mapper
 		mapper = MultiHealpixMapper(CONF, PATH_SYST, ncores=ncores)
 
-		infile = f'{OUT}{cf.metasplit[:-(lenext+1)]}_{b}.fits'
+		infile = f'{cf.path.data}{cf.data_files.metadata[:-5]}_{fd}_{b}.fits'
 		mapper(infile, bands=b, clear_intermediate_files=True)
 		
 	
