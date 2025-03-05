82650ad4aa5e6abbe080bf34c888034b3f70c152
Thomas Cornish <tomcornish1996@gmail.com>
2025-03-05 18:39:01 +0000

    get_data.py now PEP 8 compliant

--- data_query/get_data.py
+++ data_query/get_data.py
@@ -3,12 +3,10 @@ import os
 import sys
 from astropy.table import Table
 import numpy as np
-#add parent directory to path and import config parameters
-sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from configuration import PipelineConfig as PC
 from output_utils import colour_string
 
-### SETTINGS ###
+# SETTINGS #
 config_file = sys.argv[1]
 cf = PC(config_file, stage='getData')
 
@@ -18,49 +16,53 @@ cf = PC(config_file, stage='getData')
 #                              #
 ################################
 
-#directory in which the SQL queries will be saved
+# Directory in which the SQL queries will be saved
 PATH_SQL = 'sql_files/'
 if not os.path.exists(PATH_SQL):
-	os.system(f'mkdir -p {PATH_SQL}')
+    os.system(f'mkdir -p {PATH_SQL}')
 
-#Per-frame metadata
-write_frames(cf.dr, f'{PATH_SQL}frames_wide.sql', submit=cf.submit, dir_out=cf.paths.data, do_download=cf.download)
+# Per-frame metadata
+write_frames(cf.dr, f'{PATH_SQL}frames_wide.sql', submit=cf.submit,
+             dir_out=cf.paths.data, do_download=cf.download)
 
 
-#retrieve the field names and boundary coordinates
+# Retrieve the field names and boundary coordinates
 bounds_file = './field_counts_and_boundaries.fits'
 t_bounds = Table.read(bounds_file).to_pandas().set_index('field')
 
-#WIDE fields
+# WIDE fields
 for fd in cf.get_subfields():
-	print(colour_string(fd, 'purple'))
-	fld = fd.encode('utf-8')
-	#get the number of objects
-	Nobj = t_bounds.loc[fld]['countof']
-	#get the min and max Dec.
-	dec_min = t_bounds.loc[fld]['dec_min']
-	dec_max = t_bounds.loc[fld]['dec_max']
+    print(colour_string(fd, 'purple'))
+    fld = fd.encode('utf-8')
+    # Get the number of objects
+    Nobj = t_bounds.loc[fld]['countof']
+    # Get the min and max Dec.
+    dec_min = t_bounds.loc[fld]['dec_min']
+    dec_max = t_bounds.loc[fld]['dec_max']
 
-	#calculate how many parts the catalogue should be split into
-	nparts = int(np.ceil(Nobj / cf.Nmax))
+    # Calculate how many parts the catalogue should be split into
+    nparts = int(np.ceil(Nobj / cf.Nmax))
 
+    if nparts > 1:
+        # Calculate the width of each part in declination
+        ddec = (dec_max - dec_min) / nparts
+        for i in range(nparts):
+            # Determine the min and max Dec. for the current part
+            decmin_now = dec_min + (i * ddec)
+            decmax_now = decmin_now + ddec
 
-	if nparts > 1:
-		#calculate the width of each part in declination
-		ddec = (dec_max - dec_min) / nparts
-		for i in range(nparts):
-			#determine the min and max Dec. for the current part
-			decmin_now = dec_min + (i * ddec)
-			decmax_now = decmin_now + ddec
-			
-			#write and submit a query for the data in this sub-field
-			write_fieldsearch(cf.dr, fd, f'{PATH_SQL}run_{fd.upper()}_part{i+1:02d}.sql', dir_out=cf.paths.data, do_photoz=cf.photoz, mizuki_mstar_sfrs=cf.mizuki_mstar_sfrs, submit=cf.submit, apply_cuts=cf.apply_cuts, strict_cuts=cf.strict_cuts, dec_range=[decmin_now,decmax_now], do_download=cf.download, part=i+1)
-	else:
-		write_fieldsearch(cf.dr, fd, f'{PATH_SQL}run_{fd.upper()}.sql', dir_out=cf.paths.data, do_photoz=cf.photoz, mizuki_mstar_sfrs=cf.mizuki_mstar_sfrs, submit=cf.submit, apply_cuts=cf.apply_cuts, strict_cuts=cf.strict_cuts, do_download=cf.download)
-
-
-
-
-	
-
-
+            # Write and submit a query for the data in this sub-field
+            write_fieldsearch(cf.dr, fd,
+                              f'{PATH_SQL}run_{fd.upper()}_part{i+1:02d}.sql',
+                              dir_out=cf.paths.data, do_photoz=cf.photoz,
+                              mizuki_mstar_sfrs=cf.mizuki_mstar_sfrs,
+                              submit=cf.submit, apply_cuts=cf.apply_cuts,
+                              strict_cuts=cf.strict_cuts,
+                              dec_range=[decmin_now, decmax_now],
+                              do_download=cf.download, part=i+1)
+    else:
+        write_fieldsearch(cf.dr, fd, f'{PATH_SQL}run_{fd.upper()}.sql',
+                          dir_out=cf.paths.data, do_photoz=cf.photoz,
+                          mizuki_mstar_sfrs=cf.mizuki_mstar_sfrs,
+                          submit=cf.submit, apply_cuts=cf.apply_cuts,
+                          strict_cuts=cf.strict_cuts, do_download=cf.download)
