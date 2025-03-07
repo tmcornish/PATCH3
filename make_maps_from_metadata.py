e6b492314da480bbea3752bc01eabe7469531991
Thomas Cornish <tomcornish1996@gmail.com>
2025-03-07 15:47:49 +0000

    make_maps_from_metadata now PEP8 compliant

--- make_maps_from_metadata.py
+++ make_maps_from_metadata.py
@@ -1,8 +1,8 @@
-#####################################################################################################
+###############################################################
 # - Creates maps of various systematics using frame metadata.
-#####################################################################################################
+###############################################################
 
-#TODO: Try to determine a better way of configuring for different machines.
+# TODO: Try to determine a better way of configuring for different machines.
 
 import os
 import sys
@@ -11,76 +11,73 @@ from decasu.multi_healpix_mapper import MultiHealpixMapper
 from decasu.configuration import Configuration
 from output_utils import colour_string
 
-### SETTINGS ###
+# SETTINGS #
 config_file = sys.argv[1]
 cf = PC(config_file, stage='makeMapsFromMetadata')
 
-
-
 #######################################################
-###############    START OF SCRIPT    #################
+#                  START OF SCRIPT                    #
 #######################################################
 
-#retrieve the path of the Decasu config file
+# Retrieve the path of the Decasu config file
 configfile = cf.auxfiles.decasu_config
-#create Configuration object for Decasu
+# Create Configuration object for Decasu
 CONF = Configuration.load_yaml(configfile)
 
-#overwrite the output file basenames and NSIDE to match pipeline config
+# Overwrite the output file basenames and NSIDE to match pipeline config
 CONF.outbase = f'decasu_nside{cf.nside_hi}'
 CONF.nside = cf.nside_hi
-#do the same with replacements for band names
+# Do the same with replacements for band names
 for k in cf.bands.altnames:
-	for j in cf.bands.altnames[k]:
-		CONF.band_replacement[j] = k
+    for j in cf.bands.altnames[k]:
+        CONF.band_replacement[j] = k
 
-#the rest of the script now depends on whether it is being run locally or on glamdring
+# Rest of script now depends on whether it is being run locally or on glamdring
 if cf.platform == 'local':
-	#number of cores to use
-	import multiprocessing as mp
-	ncores = min(mp.cpu_count()-1, cf.ncores)
-	#cycle through the fields being analysed
-	for fd in cf.fields:
-		print(colour_string(fd.upper(), 'orange'))
-		#directory containing outputs for this field
-		OUT = cf.paths.out + fd + '/'
-		PATH_SYST = OUT + 'systmaps/'
+    # Number of cores to use
+    import multiprocessing as mp
+    ncores = min(mp.cpu_count()-1, cf.ncores)
+    # Cycle through the fields being analysed
+    for fd in cf.fields:
+        print(colour_string(fd.upper(), 'orange'))
+        # Directory containing outputs for this field
+        OUT = cf.paths.out + fd + '/'
+        PATH_SYST = OUT + 'systmaps/'
 
-		#set up Decasu mapper
-		mapper = MultiHealpixMapper(CONF, PATH_SYST, ncores=ncores)
+        # Set up Decasu mapper
+        mapper = MultiHealpixMapper(CONF, PATH_SYST, ncores=ncores)
 
-		#check if metadata was split by filter
-		if cf.split_by_band:
-			for b in cf.bands:
-				infile = f'{cf.path.data}{cf.data_files.metadata[:-5]}_{fd}_{b}.fits'
-				#run the Decasu mapper for each band individually
-				mapper(infile, bands=b, clear_intermediate_files=True)
-		else:
-			infile = f'{cf.path.data}{cf.data_files.metadata[:-5]}_{fd}.fits'
-			bands = ','.join(cf.bands)
-			#run the Decasu mapper for all bands
-			mapper(infile, bands=b, clear_intermediate_files=True)
+        # Check if metadata was split by filter
+        if cf.split_by_band:
+            for b in cf.bands:
+                infile = f'{cf.path.data}'\
+                         f'{cf.data_files.metadata[:-5]}_{fd}_{b}.fits'
+                # Run the Decasu mapper for each band individually
+                mapper(infile, bands=b, clear_intermediate_files=True)
+        else:
+            infile = f'{cf.path.data}{cf.data_files.metadata[:-5]}_{fd}.fits'
+            bands = ','.join(cf.bands)
+            # Run the Decasu mapper for all bands
+            mapper(infile, bands=b, clear_intermediate_files=True)
 
 else:
-	#retrieve the band for which the script is being run from the system arguments
-	b = sys.argv[2]
-	#environment variable specifying number of CPUs will differ on NERSC and glamdring
-	if cf.platform == 'nersc':
-		import multiprocessing as mp
-		ncores = mp.cpu_count() - 1
-	else:
-		ncores = int(os.getenv('SLURM_NTASKS_PER_NODE'))
-	#cycle through the fields being analysed
-	for fd in cf.fields:
-		print(colour_string(fd.upper(), 'orange'))
-		#directory containing outputs for this field
-		OUT = cf.paths.out + fd + '/'
-		PATH_SYST = OUT + 'systmaps/'
+    # Retrieve band for which the script is being run from the system arguments
+    b = sys.argv[2]
+    # Environment variable specifying no. of CPUs differs on NERSC vs glamdring
+    if cf.platform == 'nersc':
+        import multiprocessing as mp
+        ncores = mp.cpu_count() - 1
+    else:
+        ncores = int(os.getenv('SLURM_NTASKS_PER_NODE'))
+    # Cycle through the fields being analysed
+    for fd in cf.fields:
+        print(colour_string(fd.upper(), 'orange'))
+        # Directory containing outputs for this field
+        OUT = cf.paths.out + fd + '/'
+        PATH_SYST = OUT + 'systmaps/'
 
-		#set up Decasu mapper
-		mapper = MultiHealpixMapper(CONF, PATH_SYST, ncores=ncores)
+        # Set up Decasu mapper
+        mapper = MultiHealpixMapper(CONF, PATH_SYST, ncores=ncores)
 
-		infile = f'{cf.path.data}{cf.data_files.metadata[:-5]}_{fd}_{b}.fits'
-		mapper(infile, bands=b, clear_intermediate_files=True)
-		
-	
+        infile = f'{cf.path.data}{cf.data_files.metadata[:-5]}_{fd}_{b}.fits'
+        mapper(infile, bands=b, clear_intermediate_files=True)
