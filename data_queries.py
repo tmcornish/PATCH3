22b4429a4599a7de68d44cc0e5108eebaba139a4
Thomas Cornish <tomcornish1996@gmail.com>
2026-08-21 17:59:55 +0100

    Bug fix when calling super()

--- data_queries.py
+++ data_queries.py
@@ -18,7 +18,7 @@ class queryBase(baseStage):
         self,
         config_file,
     ):
-        super().__init__(self, config_file)
+        super().__init__(config_file)
         self.queries = []
         self.outfiles = []
         # Output directory fro SQL query files (NOT the data itself)
